7b0824da1f2a3de14ee8a3047a2c9cdfd9a93126
Konlawat Chuabswasdi <6410301016@cdti.ac.th>
2024-07-05 11:20:21 +0700

    Add new breakfast

---
+++ assignment4/breakfast04.py
@@ -0,0 +1,27 @@
+# Asynchronous breakfast
+import asyncio
+from time import sleep, time
+
+async def make_coffee():
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5)
+    print("coffee: ready")
+
+async def fry_eggs():
+    print("egg: prepare ingridients")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3)
+    print("eggs: ready")
+
+async def main():
+    start = time()
+    await asyncio.gather(make_coffee(), fry_eggs())
+    print(f"breakfast is ready in {time()-start} min")
+
+asyncio.run(main())
+    
+
+
