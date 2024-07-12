52d43197338823b7b352e3daee149e2b06957cb3
Konlawat Chuabswasdi <6410301016@cdti.ac.th>
2024-07-12 10:48:17 +0700

    add comment and task 6

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -2,23 +2,31 @@ from random import random
 import asyncio
 
 async def task_coro(arg): 
-    # random value number
+    # generate a random value between 0 and 1 
     value = random()
+    # show what task to do
     print(f"Microwave ({arg}): Cooking... {1+value}")
+    # block for a moment = 1 + random
     await asyncio.sleep(1+value)
+    # report the value
     print(f'Microwave ({arg}): Finish')
-    # return value from function
+    # return 2 value from function
     return(arg, value+1)
 
 async def main():
     # Create list of dish
     cook = ['Rice', 'Noodle', 'Curry']
-    tasks = [asyncio.create_task(task_coro(cook[i])) for i in range(0,3)]
+    # create many tasks
+    tasks = [asyncio.create_task(task_coro(cook[i])) for i in range(0,len(cook))]
+    # wait for the first task to complete
     done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    # report result
     print(f'Completed task: {len(done)} task.')
-    # print(f'Completed task:{done.pop().get_name()}')
+    # get value from function
     value = done.pop().result()
+    # report First task and time result
     print(f'- {value[0]} is completed in {value[1]}')
+    # report result
     print(f'Uncompleted task: {len(pending)} tasks.')
 
 asyncio.run(main())
--- assignment5/asyncio06.py
+++ assignment5/asyncio06.py
@@ -1,2 +1,20 @@
 from random import random
 import asyncio
+
+async def task_coro(task, arg): 
+    # show what task to do
+    print(f"Microwave ({task}): Cooking... {arg}")
+    # block for a moment = 1 + random
+    await asyncio.sleep(arg)
+    # report the value
+    print(f'Microwave ({arg}): Finish')
+    # return 2 value from function
+    return(task, arg)
+
+async def main():
+    # Create list of dish
+    cook = ['Rice', 'Noodle', 'Curry']
+    
+    # print(f'Completed task:{done.pop().get_name()}')
+    # random value number
+    value = random()
