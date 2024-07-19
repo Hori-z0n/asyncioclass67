003bdf1cde71fcd28675d0e3f6d7e538dff9c191
Konlawat Chuabswasdi <6410301016@cdti.ac.th>
2024-07-19 08:42:35 +0700

    new

--- assignment5/asyncio06.py
+++ assignment5/asyncio06.py
@@ -1,20 +1,31 @@
-from random import random
 import asyncio
+from random import random
 
-async def task_coro(task, arg): 
+async def cook(food, t): 
     # show what task to do
-    print(f"Microwave ({task}): Cooking... {arg}")
+    print(f"Microwave ({food}): Cooking {t} seconds...")
     # block for a moment = 1 + random
-    await asyncio.sleep(arg)
+    await asyncio.sleep(t)
     # report the value
-    print(f'Microwave ({arg}): Finish')
+    print(f'Microwave ({food}): Finish')
     # return 2 value from function
-    return(task, arg)
+    return f'{food} is completed in {t}'
 
 async def main():
     # Create list of dish
-    cook = ['Rice', 'Noodle', 'Curry']
-    
+    tasks = [asyncio.create_task(cook('Rice', 1 + random())),
+             asyncio.create_task(cook('Noodle', 1 + random())),
+             asyncio.create_task(cook('Curry', 1 + random()))]
+    # wait for the first task to complete
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    # report result
+    print(f'Completed task: {len(done)} task.')
+    for completed_task in done:
+        print(f' - {completed_task.result()}.')
     # print(f'Completed task:{done.pop().get_name()}')
+    print(f'Uncompleted task: {len(pending)} tasks.')
     # random value number
     value = random()
+
+if __name__=='__main__':
+    asyncio.run(main())
